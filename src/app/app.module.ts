0185565ccd37a13e400605ae123b0fe7d7115448
Gustavo Decker <gustavdz@gmail.com>
2018-04-13 12:29:46 -0500

    monitoreo de errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,33 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { firebaseConfig } from './credentials';
 import { FirestoreProvider } from '../providers/firestore/firestore';
 
+import { Pro } from '@ionic/pro';
+import { Injectable, Injector } from '@angular/core';
+
+Pro.init('24567ae1', {
+    appVersion: '0.0.1'
+})
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+    ionicErrorHandler: IonicErrorHandler;
+
+    constructor(injector: Injector) {
+        try {
+            this.ionicErrorHandler = injector.get(IonicErrorHandler);
+        } catch(e) {
+            // Unable to get the IonicErrorHandler provider, ensure
+            // IonicErrorHandler has been added to the providers list below
+        }
+    }
+
+    handleError(err: any): void {
+        Pro.monitoring.handleNewError(err);
+        // Remove this if you want to disable Ionic's auto exception handling
+        // in development mode.
+        this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+    }
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -32,7 +59,9 @@ import { FirestoreProvider } from '../providers/firestore/firestore';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    FirestoreProvider
+    FirestoreProvider,
+    IonicErrorHandler,
+    [{ provide: ErrorHandler, useClass: MyErrorHandler }]
   ]
 })
 export class AppModule {}
